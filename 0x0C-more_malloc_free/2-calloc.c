8ae1d74c2002c2cc77a3949a67ed8af9e1cc4eb0
nurudeen ibrahim <Iakinola204@gmail.com>
2022-05-09 13:09:55 -0700

    task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * _memset - main
+ * @s: main
+ * @b: main
+ * @n: main
+ * Return: main
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+for (i = 0; i < n; i++)
+{
+s[i] = b;
+}
+return (s);
+}
+/**
+ * _calloc - main
+ * @nmemb: main
+ * @size: main
+ * Return: main
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+void *p;
+if (nmemb == 0 || size == 0)
+return (NULL);
+p = malloc(nmemb * size);
+if (p == NULL)
+return (NULL);
+_memset(p, 0, (nmemb * size));
+return (p);
+}
