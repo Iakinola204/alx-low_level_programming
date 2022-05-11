54b5e4315a1cbec90f578f004c2b2443b88e8afc
nurudeen ibrahim <Iakinola204@gmail.com>
2022-05-11 13:21:48 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef FILE_DOG
+#define FILE_DOG
+/**
+ * struct dog - main
+ * @name: main
+ * @age: main
+ * @owner: main
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
