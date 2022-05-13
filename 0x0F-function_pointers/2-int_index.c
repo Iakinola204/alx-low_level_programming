9f50ff7fb82d5b5ed7ba540c9d01a42c993600ac
nurudeen ibrahim <Iakinola204@gmail.com>
2022-05-13 13:39:48 -0700

    task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+/**
+ * int_index - main
+ * @array: main
+ * @size: main
+ * @cmp: main
+ * Return: main
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+if (array && cmp)
+{
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]) != 0)
+return (i);
+}
+}
+return (-1);
+}
