366a48af07da6131ef34de7128b569b6924259d6
nurudeen ibrahim <Iakinola204@gmail.com>
2022-05-13 14:11:23 -0700

    task r4

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,25 +1,28 @@
 #include "function_pointers.h"
+
 /**
- *main - main
- *@argc: main
- *@argv: main
- *Return: main
+ *main -  prints the opcodes of its own main function.
+ *@argc: integer value.
+ *@argv: character value.
+ *
+ *Return: 0(success)
  */
 int main(int argc, char *argv[])
 {
-int i;
-if (argc != 2)
-{
-printf("Error\n");
-exit(1);
-}
-if (atoi(argv[1]) < 0)
-{
-printf("Error\n");
-exit(2);
-}
-for (i = 0; i < atoi(argv[1]) - 1; i++)
-printf("%02hhx ", ((char *)main)[i]);
-printf("%02hhx\n", ((char *)main)[i]);
-return (0);
+	int i;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (atoi(argv[1]) < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	for (i = 0; i < atoi(argv[1]) - 1; i++)
+		printf("%02hhx ", ((char *)main)[i]);
+	printf("%02hhx\n", ((char *)main)[i]);
+	return (0);
 }
