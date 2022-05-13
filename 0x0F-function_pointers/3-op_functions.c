72193234e1412468478cf643efecf32a23f6c7fc
nurudeen ibrahim <Iakinola204@gmail.com>
2022-05-13 14:00:19 -0700

    task 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - main
+ * @a: main
+ * @b: main
+ * Return: main
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+/**
+ * op_sub - main
+ * @a: main
+ * @b: main
+ * Return: main
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+/**
+ * op_mul - main
+ * @a: main
+ * @b: main
+ * Return: main
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+/**
+ * op_div - main
+ * @a: main
+ * @b: main
+ * Return: main
+ */
+int op_div(int a, int b)
+{
+return (a / b);
+}
+/**
+ * op_mod - main
+ * @a: main
+ * @b: main
+ * Return: main
+ */
+int op_mod(int a, int b)
+{
+return (a % b);
+}
