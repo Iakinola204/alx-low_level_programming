b54171c90e80c135a56715bb3658d5f0f24b7f2c
nurudeen ibrahim <Iakinola204@gmail.com>
2022-05-16 10:21:38 -0700

    task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,72 +1,27 @@
 #include "variadic_functions.h"
-#include <stdlib.h>
-#include <stdio.h>
 /**
- * print_int - main
- * @list: main
+ * print_strings - main
+ * @separator: main
+ * @n: main
+ * Return: main
  */
-void print_int(va_list list)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
-printf("%d", va_arg(list, int));
-}
-/**
- * print_float - main
- * @list: main
- */
-void print_float(va_list list)
+va_list valist;
+unsigned int i;
+char *str;
+va_start(valist, n);
+for (i = 0; i < n; i++)
 {
-printf("%f", va_arg(list, double));
-}
-/**
- * print_char - main
- * @list: main
- */
-void print_char(va_list list)
-{
-printf("%c", va_arg(list, int));
-}
-/**
- * print_str - main
- * @list: main
- */
-void print_str(va_list list)
-{
-char *s = va_arg(list, char *);
-s == NULL ? printf("(nil)") : printf("%s", s);
-}
-/**
- * print_all - main
- * @format: main
- */
-void print_all(const char * const format, ...)
-{
-va_list list;
-int i = 0, j = 0;
-char *sep = "";
-printTypeStruct printType[] = {
-{ "i", print_int },
-{ "f", print_float },
-{ "c", print_char },
-{ "s", print_str },
-{NULL, NULL}
-};
-va_start(list, format);
-while (format && format[i])
-{
-j = 0;
-while (j < 4)
-{
-if (*printType[j].type == format[i])
-{
-printf("%s", sep);
-printType[j].printer(list);
-sep = ", ";
-break;
-}
-j++;
-}
-i++;
+str = va_arg(valist, char *);
+if (str)
+printf("%s", str);
+else
+printf("(nil)");
+if (i < n - 1)
+if (separator)
+printf("%s", separator);
 }
 printf("\n");
-va_end(list);
+va_end(valist);
 }
