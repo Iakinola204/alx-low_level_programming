7f3c2f68a3c980cdf339e28dcf5476e7a4212c5b
nurudeen ibrahim <Iakinola204@gmail.com>
2022-05-16 09:56:21 -0700

    task 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,17 @@
+#include "variadic_functions.h
+/**
+ * sum_them_all - main
+ * @n: main
+ * Return: main
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+va_list list;
+unsigned int i, sum = 0;
+va_start(list, n);
+if (n != 0)
+for (i = 0; i < n; sum += va_arg(list, unsigned int), i++)
+;
+va_end(list);
+return (sum);
+}
